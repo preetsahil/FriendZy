990405e8448d8f1a53286737e984ea3cdffb11d7
sahil <preetsahil289@gmail.com>
2023-12-14 02:19:11 +0530

    started with frontend (created login page)

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Login from "./pages/login/Login";
+import {Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Login.scss";
+function Login() {
+  return (
+    <div className="Login">
+      <div className="login-box">
+        <h2 className="heading">Login</h2>
+        <form action="">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="email" id="email" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" className="password" id="password" />
+
+          <input type="submit" className="submit" />
+        </form>
+
+        <p className="subheading">Do not have an account? Sign Up</p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
