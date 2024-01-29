ff1d8fbb43250157de2459b3a451d005361c011e
sahil <preetsahil289@gmail.com>
2024-01-29 23:13:18 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -14,10 +14,15 @@ const cloudinary = require("cloudinary").v2;
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
+let origin = "http://localhost:3000";
+// console.log("here env", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  origin = process.env.CORS_ORIGIN;
+}
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin,
   })
 );
 dotenv.config("./.env");
